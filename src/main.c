f718dc6aab5bedd740245d78aa00043619ba9b72
Tetejotups <tete531@hotmail.fr>
2017-08-04 10:35:41 +0200

    Correct error in conversion code

--- src/main.c
+++ src/main.c
@@ -307,8 +307,8 @@ static int patchesDone = 0;
 int asciiToHex(char c)
 {
 	if(c>='0' && c<='9'){return(c-'0');}
-	else if (c>='a' && c<= 'f') {return(c-'a');}
-	else if (c>='A' && c<= 'F') {return(c-'A');}
+	else if (c>='a' && c<= 'f') {return(c-'a'+10);}
+	else if (c>='A' && c<= 'F') {return(c-'A'+10);}
 	else {return -1;}
 }
 
